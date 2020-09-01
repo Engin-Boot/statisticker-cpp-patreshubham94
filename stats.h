ceb9196a81a3ec851b5b4e74cc5033915a6b4e0c
patreshubham94 <68467015+patreshubham94@users.noreply.github.com>
2020-09-01 21:11:11 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -1,10 +1,17 @@
 #include <vector>
+#include <iostream>
+
+class Stats {
+    double Average;
+    double Max;
+    double Min;
+};
 
 namespace Statistics {
     Stats ComputeStatistics(const std::vector<double>& );
-    
-    double FindAverage(const std::vector<double>&V);
-    double FindMax(const std::vector<double>&V);
-    double FindMin(const std::vector<double>&V);
+        
+    double FindAverage(const std::vector<double>&);
+    double FindMax(const std::vector<double>&);
+    double FindMin(const std::vector<double>&);
     
 }
