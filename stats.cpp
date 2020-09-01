972619a22069f6b2e03056cbbb8a6e9492dffee4
patreshubham94 <68467015+patreshubham94@users.noreply.github.com>
2020-09-01 20:18:36 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,35 @@
 #include "stats.h"
+#include<vector>
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
+Stats Statistics::ComputeStatistics(const std::vector<double>& V ) {
     //Implement statistics here.
+  if(V.size() == 0) {
+        Stats = NoNumber;
+      return Stats;
+  }  
+  Stats result;
+    result.Average = FindAverage(V);
+    result.Max     = FindMax(V);
+    result.Min     = FindMin(V);
+    
+    return result;
 }
+
+double FindAverage(const std::vector<double>&V) {
+    double avg = 0;
+    avg = (accumulate(V.begin(), V.end(), 0)) / V.size();
+    return avg;
+}
+
+double FindMax(const std::vector<double>&V){
+    double max = 0;
+    max = *max_element(V.begin(), V.end());
+    return max;
+}
+
+double FindMin(const std::vector<double>&V){
+    double min = 0;
+    min = *min_element(V.begin(), V.end());
+    return min;
+}
+
